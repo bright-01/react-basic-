b4ba221ad2ca4ed8d89d960063103361e6be70e8
mwchoen <cheonmyeongwon@gmail.com>
2023-05-09 21:46:14 +0900

    리액트 고급내용 - 멘토 수정 추가 삭제

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppProfile from "./AppProfile";
 import AppXY from "./AppXY";
+import AppMentor from "./AppMentor";
+import AppMentors from "./AppMentors";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppXY />
+    <AppMentors />
   </React.StrictMode>
 );
 
