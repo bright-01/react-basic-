e55f87e921c76e6bd73fbd4ac7002339edd102fc
mwchoen <cheonmyeongwon@gmail.com>
2023-05-17 07:08:31 +0900

    CSS 라이브러리 - Styled Components, tailwind

--- src/AppCss.jsx
+++ src/AppCss.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import Button1 from "./components/button/Button1";
-import Button2 from "./components/button/Button2";
+import Button1 from "./components/css/Button1";
+import Button2 from "./components/css/Button2";
+import StyledComponentsButton from "./components/css/StyledComponentsButton";
+import TailWindComponent from "./components/css/TailWindComponent";
 
-import styled, {css} from "styled-components";
-
-const Container = styled.div`
-display: flex;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${(props) =>
-   props.primary && 
-   css`
-     background: #009cd5;
-     color: white;
-   `};
-`;
 
 
 export default function AppCss() {
@@ -30,10 +12,8 @@ export default function AppCss() {
 
             <Button1></Button1>
             <Button2></Button2>
-            <Container>
-                <Button>Normal Button</Button>
-                <Button primary>Primary Button</Button>
-            </Container>
+            <StyledComponentsButton></StyledComponentsButton>
+            <TailWindComponent></TailWindComponent>
         </div>
     )
 }
